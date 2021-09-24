69fa615ba2eb28bc243b38dba87660d9bce6fce4
Jhennyfer Hernandez <91267310+jennyferherpa@users.noreply.github.com>
2021-09-24 09:54:15 -0500

    Update Empleado.java

--- PruebaBackend/src/main/java/com/prueba/tecnica/model/Empleado.java
+++ PruebaBackend/src/main/java/com/prueba/tecnica/model/Empleado.java
@@ -3,6 +3,7 @@ package com.prueba.tecnica.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.data.annotation.CreatedDate;
@@ -13,6 +14,7 @@ import javax.persistence.*;
 import java.util.Date;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @Entity
